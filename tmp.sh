1e21459d21d2705f5654296e334490275e743b59
Jonathan <43968722+LateJune@users.noreply.github.com>
2023-03-11 13:04:22 -0500

    tmp file

---
+++ tmp.sh
@@ -0,0 +1,5 @@
+sudo apt update && sudo apt upgrade
+
+echo [+] Installing apt packages
+sleep 3
+sudo apt install aircrack-ng exiftool git locate net-tools nmap python3 python3-pip smbclient traceroute vim whois zsh -y
